667b9ddb0d24387fe8d54bc3dfc4598cabdfcce6
Walter Frederick Seballos <wolt45@yahoo.com>
2014-08-13 23:13:34 +0800

    iope8
    iope8

---
+++ index.php
@@ -0,0 +1,205 @@
+ <!DOCTYPE HTML>
+<html>
+<head>
+<title>GUSTILO CLINIC & AMBULATORY CENTER</title>
+<link href="css/all.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="css/tablex.css" rel="stylesheet"  type="text/css" />
+
+</head>
+<body>
+
+<div class="row">
+	<div class="col-xs-12 col-sm-2 bs-docs-container" id="topLeft">
+		<div id="wnavtopLeft" style="border: 0px solid RED; width:16%">
+			<div class="panel-group" id="accordion">
+
+				<div class="panel panel-default">
+					<div class="panel-heading btn btn-success btn-block">
+						<a data-parent="#accordion" href="#ChartData" style="display:block; text-decoration:none">
+							PATIENT INFO</a>
+					</div>
+				
+					<div class="panel-heading btn btn-success btn-block">
+						<a data-toggle="collapse" data-parent="#accordion" href="#IOPE" style="display:block; text-decoration:none">
+							IOHPE</a>
+					</div>
+					
+					<div id="IOPE" class="panel-collapse collapse">
+						<div class="panel-body">
+							<a class="btn-warning btn-block btn-lg text-center" href="#IOPEChiefComp"> Chief Complaint </a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#IOPEEtio">Etiology </a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#IOPETreat">Treatment </a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#IOPESurgery">Surgery</a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#IOPELabs">Lab Exams</a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#IOPEMedHist">Medical History </a>
+						</div>
+					</div>
+					
+					<div class="panel-heading btn btn-danger btn-block">
+						<a data-toggle="collapse" data-parent="#accordion" href="#GENERALO" style="display:block; text-decoration:none">
+							GENERAL ORTHOPEDICS</a>
+					</div>
+					<div id="GENERALO" class="panel-collapse collapse">
+						<div class="panel-body">
+							<a class="btn-warning btn-block btn-lg text-center" href="#GOChief"> Chief Complaint </a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseTwo">Etiology </a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseThree">Physical Examination</a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseFour">Dignosis</a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseFive">Lab Exams</a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseSix">Previous Surgery</a>
+						</div>
+					</div>
+					
+					<div class="panel-heading btn btn-danger btn-block">
+						<a data-toggle="collapse" data-parent="#accordion" href="#TRAUMA"  style="display:block; text-decoration:none">
+							TRAUMA</a>
+					</div>
+					<div id="TRAUMA" class="panel-collapse collapse">
+						<div class="panel-body">
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseOne"> Chief Complaint </a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseTwo">Etiology </a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseThree">Treatment </a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseFour">Surgery</a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseFive">Lab Exams</a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseSix">Medical History </a>
+						</div>
+					</div>
+					
+
+					<div class="panel-heading btn btn-danger btn-block">
+						<a data-toggle="collapse" data-parent="#accordion" href="#HIPKNEEReqs"  style="display:block; text-decoration:none">
+							HIP / KNEE XRAY REQUEST</a>
+					</div>
+					<div id="HIPKNEEReqs" class="panel-collapse collapse">
+						<div class="panel-body">
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseOne"> HIP/KNEE X-Ray Request</a>
+						</div>
+					</div>
+					
+					<div class="panel-heading btn btn-active btn-block">
+						<a data-toggle="collapse" data-parent="#accordion" href="#IOPEHIP"  style="display:block; text-decoration:none">
+							IOPE HIP MODULE</a>
+					</div>
+					<div id="IOPEHIP" class="panel-collapse collapse">
+						<div class="panel-body">
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseOne"> PHYSICAL EXAM</a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseTwo">X-RAY FINDINGS</a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseThree">DIAGNOSIS</a>
+						</div>
+					</div>
+					
+
+					
+					<div class="panel-heading btn btn-active btn-block">
+						<a data-toggle="collapse" data-parent="#accordion" href="#IOPEHIPPreOp"  style="display:block; text-decoration:none">
+							HIP REPL PRE-OP ORDERS</a>
+					</div>
+					<div id="IOPEHIPPreOp" class="panel-collapse collapse">
+						<div class="panel-body">
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseOne">HIP REPLACEMENT PRE OPERATIVE FORM</a>
+						</div>
+					</div>
+
+					
+					
+					<div class="panel-heading btn btn-active btn-block">
+						<a data-toggle="collapse" data-parent="#accordion" href="#IOPEHIPOpOrds"  style="display:block; text-decoration:none">
+							HIP REPL OP ORDERS</a>
+					</div>
+					<div id="IOPEHIPOpOrds" class="panel-collapse collapse">
+						<div class="panel-body">
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseOne">HIP POST OPERATIVE FORM</a>
+						</div>
+					</div>
+					
+					
+					
+					
+					
+					<div class="panel-heading btn btn-info btn-block">
+						<a data-toggle="collapse" data-parent="#accordion" href="#KNEE"  style="display:block; text-decoration:none">
+							IOPE KNEE MODULE</a>
+					</div>
+					<div id="KNEE" class="panel-collapse collapse">
+						<div class="panel-body">
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseOne"> Physical Examination</a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseTwo">Radiographic Findings</a>
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseThree">Diagnosis</a>
+						</div>
+					</div>
+					
+					
+					
+					
+					<div class="panel-heading btn btn-info btn-block">
+						<a data-toggle="collapse" data-parent="#accordion" href="#KRPO"  style="display:block; text-decoration:none">
+							KNEE REPL PRE-OP ORDERS</a>
+					</div>
+					<div id="KRPO" class="panel-collapse collapse">
+						<div class="panel-body">
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseOne">KNEE PRE-OPERATIVE FORM</a>
+						</div>
+					</div>
+					
+					
+					
+					<div class="panel-heading btn btn-info btn-block">
+						<a data-toggle="collapse" data-parent="#accordion" href="#KROO"  style="display:block; text-decoration:none; font-color: #000000">
+							KNEE REPL OP ORDERS</a>
+					</div>
+					<div id="KROO" class="panel-collapse collapse">
+						<div class="panel-body">
+							<a class="btn-warning btn-block btn-lg text-center" href="#collapseOne">KNEE POST-OPERATIVE FORM</a>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="col-xs-12 col-sm-10">
+		<div id="ChartData">
+			<div class="panel panel-success">
+				<div class="panel-heading" style="height:105px; border:1px solid #cff">
+					<div style="background-color: #a5a; width:120px; float:left; margin: 0 20px 20px 0">
+						<img src="images/1.4.DSC_0281.JPG" class="img-responsive img-rounded"
+							alt="Responsive image" width="320px" />
+					</div>	
+					<div style="float:left">
+						<table class="table table-bordered">
+							<tr>
+								<td class="info">Jan 5, 2014 chart: 123456</td>
+								<td class="danger">Seballos, Joanna Grace</td>
+								<td class="active">f/16</td>
+								<td class="active">Student</td>
+							</tr>
+							<tr>
+								<td class="success" colspan=9>#12 Gallo St, Manapla</td>
+							</tr>
+						</table>
+					</div>		
+				</div>
+			</div>
+		</div>
+		
+		<?php
+		include "iope.chiefcomp.php";
+		include "iope.etiology.php";
+		include "iope.treatment.php";
+		include "iope.surgery.php";
+		include "iope.labexams.php";
+		include "iope.medhist.php";
+		?>
+		
+
+	</div>
+		
+<script src="lib/jquery-1.7.1.min.js"></script>
+<script src="lib/bootstrap.min.js"></script>
+<script src="lib/jquery.min.js" type="text/javascript"></script>
+<script src="lib/jquery.main.js" type="text/javascript"></script>
+
+</body>
+</html>
